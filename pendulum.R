086e2994c824070a80e3b8e23032ede122e882a4
Brendon J. Brewer <brendon.brewer@gmail.com>
2026-05-19 11:54:57 +1200

    Save results and make a plot.

--- pendulum.R
+++ pendulum.R
@@ -3,12 +3,15 @@
 state = c(position=1, velocity=0)
 dt = 0.01
 
-time = 10
+time = 100
 steps = floor(time/dt)
-
+keep = numeric(steps)
 for(i in 1:steps)
 {
     accel = -sin(state["position"])
     state["position"] = state["position"] + dt*state["velocity"]
     state["velocity"] = state["velocity"] + dt*accel
+    keep[i] = state["position"]
 }
+
+plot((1:steps)*dt, keep, type="l", xlab="Time", ylab="Position")
