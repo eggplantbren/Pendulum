5a515dbd034159f181e7f0de59844d5f1b7324b2
Brendon J. Brewer <brendon.brewer@gmail.com>
2026-05-19 11:53:02 +1200

    Added the loop

--- pendulum.R
+++ pendulum.R
@@ -2,3 +2,13 @@
 
 state = c(position=1, velocity=0)
 dt = 0.01
+
+time = 10
+steps = floor(time/dt)
+
+for(i in 1:steps)
+{
+    accel = -sin(state["position"])
+    state["position"] = state["position"] + dt*state["velocity"]
+    state["velocity"] = state["velocity"] + dt*accel
+}
